835c1b0b4756fcc715737c105f25f65efc8f0a30
cx2dqnngch-max <cx2dqnngch@privaterelay.appleid.com>
2026-03-19 09:33:17 -0400

    Update build.sh with legacy-peer-deps flag

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo '=== STEP 1: Setting Expo token ==='
+echo '=== STEP 1: Setting token ==='
 export EXPO_TOKEN=4HEWsaWWY6EsQg8akK-I5OuEf9LEmhNEu_aINIsv
-echo '=== STEP 2: Installing packages (2 mins) ==='
+echo '=== STEP 2: Installing packages ==='
 npm install --legacy-peer-deps
-echo '=== STEP 3: Starting EAS build ==='
+echo '=== STEP 3: Launching build ==='
 npx eas-cli build --platform android --profile production
