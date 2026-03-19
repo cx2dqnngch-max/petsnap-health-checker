883870064ea406be9c9f495f0e4296818df632b3
cx2dqnngch-max <cx2dqnngch@privaterelay.appleid.com>
2026-03-19 08:54:25 -0400

    Update build script with npm install

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export EXPO_TOKEN=4HEWsaWWY6EsQg8akK-I5OuEf9LEmhNEu_aINIsv
-echo 'Logged in to Expo...'
+echo 'Installing dependencies...'
+npm install
+echo 'Starting build...'
 npx eas-cli build --platform android --profile production
