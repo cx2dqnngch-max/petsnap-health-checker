631f4cb023eb1a4f2e531d396c6cb69f64b7c04a
cx2dqnngch-max <cx2dqnngch@privaterelay.appleid.com>
2026-03-19 08:56:44 -0400

    Fix: add npm install with legacy peer deps

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo '=== STEP 1: Setting Expo token ==='
 export EXPO_TOKEN=4HEWsaWWY6EsQg8akK-I5OuEf9LEmhNEu_aINIsv
-echo 'Installing dependencies...'
-npm install
-echo 'Starting build...'
+echo '=== STEP 2: Installing packages (2 mins) ==='
+npm install --legacy-peer-deps
+echo '=== STEP 3: Starting EAS build ==='
 npx eas-cli build --platform android --profile production
